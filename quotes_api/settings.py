801b34157f27d70932f2b0771e2a225ee2e510ad
HumbertoIsraelLV <63939946+HumbertoIsraelLV@users.noreply.github.com>
2022-03-20 15:38:58 -0600

    Additions to make a heroku deployment

--- quotes_api/settings.py
+++ quotes_api/settings.py
@@ -26,16 +26,21 @@ f.close()
 SECRET_KEY = secret['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
+    ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 ]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    #To disable Django’s static file handling and allow WhiteNoise to take over
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    #...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+WHITENOISE_USE_FINDERS = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 
 # Default primary key field type
